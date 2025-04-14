d437cc8400d5ef45ce6b24da0a5ef0d21830d8e7
AxelleBdd <axbaudinaud@gmail.com>
2025-04-14 15:48:41 +0200

    testing chrome tuto extensions

---
+++ tuto/service-worker.js
@@ -0,0 +1,10 @@
+import './sw-omnibox.js';
+import './sw-tips.js';
+
+// Send tip to content script via messaging
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting === 'tip') {
+    chrome.storage.local.get('tip').then(sendResponse);
+    return true;
+  }
+});
