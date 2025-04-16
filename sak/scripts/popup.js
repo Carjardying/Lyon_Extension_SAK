970c2c720408a7a609aa895030ab9cb13d19a1fc
chazera <hazera.c@gmail.com>
2025-04-16 17:23:57 +0200

    tab url query

--- sak/scripts/popup.js
+++ sak/scripts/popup.js
@@ -4,16 +4,23 @@ const goToButton = document.querySelector('#go-to-button');
 const addButton = document.querySelector('#add-button');
 const sendButton = document.querySelector('#send-button');
 const closeButton = document.querySelector('#close-button');
+const tabUrl = document.querySelector('#tab-url');
 
 goToButton.addEventListener("click", (tab) => {
     chrome.tabs.create({
-        url: "kanban.html" 
+        url: "kanban.html"
     });
 });
 
 addButton.addEventListener("click", () => {
     mainPage.style.display = "none";
     addingPage.style.display = "block";
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const currentTab = tabs[0];
+        const url = currentTab.url;
+
+        tabUrl.textContent = url;
+    });
 });
 
 closeButton.addEventListener("click", () => {
