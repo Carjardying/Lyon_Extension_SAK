9c8ba65bc41b3ba6f6dd038fc47af3ef1af33613
chazera <hazera.c@gmail.com>
2025-04-16 16:17:19 +0200

    managing add button in popup

--- sak/scripts/popup.js
+++ sak/scripts/popup.js
@@ -1,7 +1,28 @@
-const openButton = document.querySelector('#open-button');
+const mainPage = document.querySelector('#main-page');
+const addingPage = document.querySelector('#adding-page');
+const goToButton = document.querySelector('#go-to-button');
+const addButton = document.querySelector('#add-button');
+const sendButton = document.querySelector('#send-button');
+const closeButton = document.querySelector('#close-button');
 
-openButton.addEventListener("click", (tab) => {
+goToButton.addEventListener("click", (tab) => {
     chrome.tabs.create({
-        url: "https://morgan.corroyer.fr/"
+        // changer url pour kanban.html
+        url: "https://morgan.corroyer.fr/" 
     });
+});
+
+addButton.addEventListener("click", () => {
+    mainPage.style.display = "none";
+    addingPage.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+    mainPage.style.display = "block";
+    addingPage.style.display = "none";
+});
+
+sendButton.addEventListener("click", () => {
+    // faire code ...
+    console.log("ok envoi");
 });
